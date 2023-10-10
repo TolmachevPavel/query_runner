5d3ef2f0b275c111f76f0254d3d4f14887995cf7
Pavel Tolmachev <p.tolmachev@postgrespro.ru>
2023-10-10 10:08:10 +0300

    Добавил уточняющие комментарии

--- add_first_row.sh
+++ add_first_row.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for var in $(ls ~/join-order-benchmark)
+# Добавляю одну строку в начало...
+for var in $(ls ~/join-order-benchmark) #...всех файлов в этой директории
   do
-      sed -i -e '1 s/^/EXPLAIN ANALYZE\n/;' ~/join-order-benchmark/$var
+      sed -i -e '1 s/^/EXPLAIN ANALYZE\n/;' ~/join-order-benchmark/$var # Строку вот эту: EXPLAIN ANALYZE
   done
